9e01dd22e1a497288d614d2030f5d88e007ec1de
regina sanchez <nina.sanchez13@outlook.com>
2024-01-24 21:18:50 -0800

    Added errors

---
+++ debugging.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int user-input;
+    int sum = 0
+
+    // using a for loop to get numbers from the user 5 times
+    for (i = 0; i < 5; ++i) {
+        cout << "Enter a number: ";
+        cin >> userInput;
+
+        // add the entered number to the sum
+        sum += number:
+    }
+
+    cout << "Sum of the entered numbers: " << sum << endl;
+
+    return
+}
+
