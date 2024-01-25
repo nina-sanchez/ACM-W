8e60f9eb100f8812a369695ed7c89a98d0e8637b
regina sanchez <nina.sanchez13@outlook.com>
2024-01-24 21:18:21 -0800

    File with while-loop

---
+++ while-loop.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int i = 1;
+
+    // using a while loop to print numbers from 1 to 5
+    while (i <= 5) {
+        cout << "Number: " << i << endl;
+        ++i; // this means i = i + 1
+    }
+
+    return 0;
+}
+
