16af30cf65276656bce6a52cbba2b00813e27e98
regina sanchez <nina.sanchez13@outlook.com>
2024-01-24 21:17:24 -0800

    File with for-loop

---
+++ for-loop.cpp
@@ -0,0 +1,12 @@
+#include <iostream> 
+
+using namespace std; 
+
+int main() {
+    // Using a for loop to print numbers from 1 to 5
+    for (int i = 1; i <= 5; ++i) {
+        cout << "Number: " << i << endl;
+    }
+
+    return 0;
+}
